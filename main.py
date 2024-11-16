cdd45d9410e391a5864b4994a5faaf3ef0b5f2eb
Joohyung-Han <65501090+Joohyung-Han@users.noreply.github.com>
2024-11-17 05:31:58 +0900

    fix: do not load on hparams at main

--- main.py
+++ main.py
@@ -25,23 +25,6 @@ from src.pipelines.pipeline import train, test, predict, tune
 def main(
     config: DictConfig,
 ) -> None:
-    if config.is_tuned == "tuned":
-        params = json.load(
-            open(
-                config.tuned_hparams_path,
-                "rt",
-                encoding="UTF-8",
-            )
-        )
-        config = OmegaConf.merge(
-            config,
-            params,
-        )
-    elif config.is_tuned == "untuned":
-        pass
-    else:
-        raise ValueError(f"Invalid is_tuned argument: {config.is_tuned}")
-
     if config.mode == "train":
         return train(config)
     elif config.mode == "test":
