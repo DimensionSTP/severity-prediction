dd32369d698319d80f77b10bf73cc1c1e2764d23
Joohyung-Han <65501090+Joohyung-Han@users.noreply.github.com>
2024-11-17 05:01:50 +0900

    feat: scripts update

--- scripts/predict.sh
+++ scripts/predict.sh
@@ -7,7 +7,7 @@ num_trials=100
 for config_name in $config_names
 do
     python main.py --config-name=$config_name.yaml \
-        mode=train \
+        mode=predict \
         is_tuned=$is_tuned \
         num_trials=$num_trials
 done
