6f58b75e3775374f63b288d15607a0e06b7368c0
DimensionSTP <ddang8jh@gmail.com>
2024-11-18 14:25:38 +0900

    add: scripts

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+config_names="lgbm xgb cb"
+is_tuned="tuned"
+num_trials=100
+
+for config_name in $config_names
+do
+    python main.py --config-name=$config_name.yaml \
+        mode=test \
+        is_tuned=$is_tuned \
+        num_trials=$num_trials
+done
